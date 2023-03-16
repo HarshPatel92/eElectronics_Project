06c66567ef3747a50cbeccd48bb6fe149a194917
HarshPatel92 <harshpatel180502@gmail.com>
2023-03-16 11:58:25 +0530

    login

--- eElectronics/user/views.py
+++ eElectronics/user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import User
 from .forms import ManagerRegisterForm,DeveloperRegisterForm
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 class ManagerRegisterView(CreateView):
@@ -11,14 +13,16 @@ class ManagerRegisterView(CreateView):
     success_url = '/'
     
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
-        if user.is_authenticated:
-            if user.is_superuser:
-                context['user_type'] = 'manager'
-            else:
-                context['user_type'] = 'regular'
-        return context
+        kwargs['user_type'] = 'manager'
+        return super().get_context_data(self, **kwargs)
+    
+    def form_valid(self, form):
+        #email = form.cleaned_data.get('email')
+        user = form.save()
+        login(self.request,user)
+        return super().form_valid(form)
+            
+        
     
 class DeveloperRegisterView(CreateView):
     model = User
@@ -27,11 +31,16 @@ class DeveloperRegisterView(CreateView):
     success_url = '/'
     
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
-        if user.is_authenticated:
-            if user.is_superuser:
-                context['user_type'] = 'developer'
-            else:
-                context['user_type'] = 'regular'
-        return context
+        kwargs['user_type'] = 'developer'
+        return super().get_context_data(**kwargs)
+        
+        
+class UserLoginView(LoginView):
+    template_name = 'user/login.html'
+    #success_url = "/"
+    
+    def get_redirect_url(self):
+        if self.request.user.is_manager1:
+            return '/manager/'
+        else:
+            return '/developer/'
